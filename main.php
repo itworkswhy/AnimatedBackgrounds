03ddaa0ca944c0bc4d360a98318b02423ef78fc2
Katie Burton <ktlou.burton@gmail.com>
2019-06-04 13:27:47 +0100

    Initial commit

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Animated Backgrounds
+Plugin URI: ""
+Description: Animated backgrounds for Wordpress
+Version: 1.0
+Author: K Burton
+Author URI: https://itworkswhy.co.uk
+*/
